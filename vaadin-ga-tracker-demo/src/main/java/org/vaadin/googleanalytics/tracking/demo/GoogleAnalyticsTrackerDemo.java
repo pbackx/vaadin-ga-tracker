bfd1a91a7641f5bb4e69f208b44bbfcc21257387
Peter Backx <peter.backx@gmail.com>
2013-12-19 17:11:50 +0100

    Maven-izing project

---
+++ vaadin-ga-tracker-demo/src/main/java/org/vaadin/googleanalytics/tracking/demo/GoogleAnalyticsTrackerDemo.java
@@ -0,0 +1,48 @@
+package org.vaadin.googleanalytics.tracking.demo;
+
+import javax.servlet.annotation.WebServlet;
+
+import org.vaadin.googleanalytics.tracking.GoogleAnalyticsTracker;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.UI;
+
+public class GoogleAnalyticsTrackerDemo extends UI {
+    private static final long serialVersionUID = 1L;
+
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = GoogleAnalyticsTrackerDemo.class)
+	public static class Servlet extends VaadinServlet {
+		private static final long serialVersionUID = 1L;
+	}
+	
+    @Override
+    protected void init(VaadinRequest request) {
+
+        // Create a tracker for vaadin.com domain.
+        GoogleAnalyticsTracker tracker = new GoogleAnalyticsTracker(
+                "UA-658457-8");
+
+        // Example: Create a tracker for vaadin.com domain.
+        // GoogleAnalyticsTracker tracker = new GoogleAnalyticsTracker(
+        // "UA-658457-8", "vaadin.com");
+
+        // attach the GA tracker to this UI
+        tracker.extend(this);
+
+        // simple view navigator sample
+        Navigator n = new Navigator(this, this);
+        n.addView("", MainView.class);
+        n.addView("second", SecondView.class);
+        n.addView("third", ThirdView.class);
+
+        // attach the tracker to the Navigator to automatically track all views
+        // To use the tracker without the Navigator, just call the
+        // tracker.trackPageview(pageId) separately when tracking is needed.
+        getNavigator().addViewChangeListener(tracker);
+
+    }
+}
